a99e89b783450886c81289a294a76bf513130d8d
Hayden Quinn <hayden@increnaut.io>
2024-09-22 20:19:30 +1000

    test: Adding more test cases around parsing numbers

--- tests/LegacyRoller.UnitTests/DiceExpressionParserTests.cs
+++ tests/LegacyRoller.UnitTests/DiceExpressionParserTests.cs
@@ -5,9 +5,13 @@ public class DiceExpressionParserTests
     [Test]
     [Arguments("1", "NUMBER(1)")]
     [Arguments("2", "NUMBER(2)")]
+    [Arguments("10", "NUMBER(10)")]
+    [Arguments("6.9", "NUMBER(6.9)")]
     public async Task Should_Parse_Input_Into_Dice_Expression(string input, string expected)
     {
         var result = DiceExpressionParser.Parse(input);
-        await Assert.That(result.ToString()).IsEqualTo(expected);
+        var actual = result.ToString();
+        
+        await Assert.That(actual).IsEqualTo(expected);
     }
 }
