49725f6af6d72aeb3af7912f7c3959c301b141f8
Hayden Quinn <hayden@increnaut.io>
2024-09-22 20:27:20 +1000

    test: Adding more test cases around parsing unary expressions

--- tests/LegacyRoller.UnitTests/DiceExpressionParserTests.cs
+++ tests/LegacyRoller.UnitTests/DiceExpressionParserTests.cs
@@ -8,6 +8,9 @@ public class DiceExpressionParserTests
     [Arguments("10", "NUMBER(10)")]
     [Arguments("6.9", "NUMBER(6.9)")]
     [Arguments("-1", "UNARY(NUMBER(1))")]
+    [Arguments("-2", "UNARY(NUMBER(2))")]
+    [Arguments("-10", "UNARY(NUMBER(10))")]
+    [Arguments("-6.9", "UNARY(NUMBER(6.9))")]
     public async Task Should_Parse_Input_Into_Dice_Expression(string input, string expected)
     {
         var result = DiceExpressionParser.Parse(input);
