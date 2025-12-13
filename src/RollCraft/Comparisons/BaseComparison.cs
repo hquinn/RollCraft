526384de2b0181a3925d9cbc7cf9402250949656
Hayden Quinn <hayden@increnaut.io>
2025-12-13 18:20:07 +1000

    fix: Made comparison evaluations thread-safe

--- src/RollCraft/Comparisons/BaseComparison.cs
+++ src/RollCraft/Comparisons/BaseComparison.cs
@@ -6,8 +6,8 @@ namespace RollCraft.Comparisons;
 internal abstract class BaseComparison<TNumber> : IComparison
     where TNumber : INumber<TNumber>
 {
-    protected int? _comparisonValue;
-    protected List<DiceRoll>? _rolls;
+    private readonly ThreadLocal<int?> _comparisonValue = new();
+    private readonly ThreadLocal<List<DiceRoll>?> _rolls = new();
     
     protected BaseComparison(DiceExpression<TNumber> comparison)
     {
@@ -18,7 +18,7 @@ internal abstract class BaseComparison<TNumber> : IComparison
 
     public Result<IRollError, (bool Success, List<DiceRoll> Rolls)> RollEquals(IRoller roller, DiceRoll roll)
     {       
-        if (_comparisonValue is null)
+        if (_comparisonValue.Value is null)
         {
             var result = Comparison.Evaluate(roller);
             
@@ -33,30 +33,30 @@ internal abstract class BaseComparison<TNumber> : IComparison
                 return new EvaluatorError("Evaluator.ComparisonError", "Comparison must be an integer!");
             }
 
-            var comparison = int.CreateSaturating(result.Value.Result);
+            var comparisonVal = int.CreateSaturating(result.Value.Result);
             
-            if (comparison < 1)
+            if (comparisonVal < 1)
             {
                 return new EvaluatorError("Evaluator.ComparisonError", "Comparison must not be less than 1!");
             }
             
-            if (comparison > roll.Sides)
+            if (comparisonVal > roll.Sides)
             {
                 return new EvaluatorError("Evaluator.ComparisonError", "Comparison must not be greater than the dice side count!");
             }
             
-            _comparisonValue = comparison;
-            _rolls = result.Value.Rolls;
+            _comparisonValue.Value = comparisonVal;
+            _rolls.Value = result.Value.Rolls;
         }
         
-        return Result<IRollError, (bool Success, List<DiceRoll> Rolls)>.Success((Compare(roll.Roll, _comparisonValue.Value), _rolls!));
+        return Result<IRollError, (bool Success, List<DiceRoll> Rolls)>.Success((Compare(roll.Roll, _comparisonValue.Value!.Value), _rolls.Value!));
     }
     
     protected abstract bool Compare(int roll, int comparisonValue);
 
     public void Reset()
     {
-        _comparisonValue = null;
-        _rolls = null;
+        _comparisonValue.Value = null;
+        _rolls.Value = null;
     }
 }
